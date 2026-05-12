cc1bcd829d2b863d1af136e244f4e6102fa784c7
luisgustavopereiraf@gmail.com <luisgustavopereiraf@gmail.com>
2026-05-12 12:36:43 -0300

    feat: minimal web app and api comunication

---
+++ apps/web/src/components/Schedule.tsx
@@ -0,0 +1,22 @@
+import type { Schedule } from '@generated/prisma/client';
+
+export async function fetchSchedules(): Promise<Schedule[]> {
+  const apiBaseUrl = process.env.API_BASE_URL!;
+
+  const data = await fetch(`${apiBaseUrl}/schedules`);
+  if (!data.ok) return [];
+
+  return data.json();
+}
+
+export function Schedule(data: Schedule) {
+  const title = `${data.openTime} - ${data.closeTime}`;
+  const info = `${data.lunchStart} - ${data.lunchEnd}`;
+
+  return (
+    <li key={data.dayOfWeek}>
+      <h2>{title}</h2>
+      <p>{info}</p>
+    </li>
+  );
+}
